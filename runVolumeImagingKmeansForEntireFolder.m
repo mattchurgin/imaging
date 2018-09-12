628098c7b8592ac28c7de31dae05b4adf5f15f5d
mattlabcomputer <mattchurgin@gmail.com>
2018-09-12 08:41:26 -0400

    added automatic creation of clusters and responses to script runVolumeImagingKmeansforEntireFolder.m

--- runVolumeImagingKmeansForEntireFolder.m
+++ runVolumeImagingKmeansForEntireFolder.m
@@ -1,16 +1,15 @@
-% run VolumeImagingkKmeans.m for all flies in a directory
-
+%% run VolumeImagingkKmeans.m for all flies in a directory
+% Matt Churgin, September 2018
 folderToRunKmeansOn='Volumes';
 
-startDir=pwd;
-currFolders = dir(pwd);
+startDir=uigetfolder(); % user selects the folder containing each fly's image folders
+currFolders = dir(startDir);
 currFolders=currFolders(3:end);
 for i=1:length(currFolders)
     actuallyAFolder(i)=currFolders(i).isdir;
 end
 currFolders(~actuallyAFolder)=[];
 
-tic
 for i=1:length(currFolders)
     cd(currFolders(i).name)
     if exist('leftLobe')
@@ -25,4 +24,38 @@ for i=1:length(currFolders)
     end
     cd(startDir)
 end
-disp(['done.  time elapsed = ' num2str(toc/3600) ' hours.'])
+disp(['done calculating kmeans.'])
+
+%% run makeClusters.m and processVolumeImagingKmeans.m for all folders
+
+folderToRunKmeansOn='Volumes';
+rawKmeansOutput='rawKmeans_80clusters_0.99fractionOfVarianceKept_50replicates.mat';
+clusterVolFile='clusterVols.mat';
+volumeAcquisitionTime=1.2;
+nChannels=2;
+
+startDir=uigetfolder(); % user selects the folder containing each fly's image folders
+currFolders = dir(startDir);
+currFolders=currFolders(3:end);
+for i=1:length(currFolders)
+    actuallyAFolder(i)=currFolders(i).isdir;
+end
+currFolders(~actuallyAFolder)=[];
+
+for i=1:length(currFolders)
+    cd(currFolders(i).name)
+    if exist('leftLobe')
+        cd(['leftLobe\' folderToRunKmeansOn])
+       [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput)
+       [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
+        cd([startDir '\' currFolders(i).name])
+    end
+    if exist('rightLobe')
+        cd(['rightLobe\'  folderToRunKmeansOn])
+       [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput)
+       [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
+        cd([startDir '\' currFolders(i).name])
+    end
+    cd(startDir)
+end
+disp(['done creating clusters and responses.'])
