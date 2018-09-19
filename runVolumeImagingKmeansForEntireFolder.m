da02682ab789e3555ab0ecb3001c88da6c12ef12
mattlabcomputer <mattchurgin@gmail.com>
2018-09-19 14:12:46 -0400

    improved batch processing of kmeans

--- runVolumeImagingKmeansForEntireFolder.m
+++ runVolumeImagingKmeansForEntireFolder.m
@@ -1,6 +1,14 @@
 %% run VolumeImagingkKmeans.m for all flies in a directory
 % Matt Churgin, September 2018
-folderToRunKmeansOn='Volumes';
+clear all
+folderToRunKmeansOn{1}='Volumes';
+folderToRunKmeansOn{2}='Volumes2';
+fractionOfVarianceTokeep=0.75;
+replicates=50;
+nkmeans=80;
+volsPerOdor=20;
+nchannels=2;
+nvolseries=13;
 
 startDir=uigetdir(); % user selects the folder containing each fly's image folders
 currFolders = dir(startDir);
@@ -13,14 +21,18 @@ currFolders(~actuallyAFolder)=[];
 for i=1:length(currFolders)
     cd(currFolders(i).name)
     if exist('leftLobe')
-        cd(['leftLobe\' folderToRunKmeansOn])
-       [greenImages redImages kmeansOut unwrappedImages unwrappedReduced meanGreenImages meanRedImages] = volumeImagingKmeans(80,20,2,13,50,0.99);
-        cd([startDir '\' currFolders(i).name])
+        for j=1:length(folderToRunKmeansOn)
+            cd(['leftLobe\' folderToRunKmeansOn{j}])
+            [greenImages redImages kmeansOut unwrappedImages unwrappedReduced meanGreenImages meanRedImages] = volumeImagingKmeans(nkmeans,volsPerOdor,nchannels,nvolseries,replicates,fractionOfVarianceTokeep);
+            cd([startDir '\' currFolders(i).name])
+        end
     end
     if exist('rightLobe')
-        cd(['rightLobe\'  folderToRunKmeansOn])
-        [greenImages redImages kmeansOut unwrappedImages unwrappedReduced meanGreenImages meanRedImages] = volumeImagingKmeans(80,20,2,13,50,0.99);
-        cd([startDir '\' currFolders(i).name])
+        for j=1:length(folderToRunKmeansOn)
+            cd(['rightLobe\'  folderToRunKmeansOn{j}])
+            [greenImages redImages kmeansOut unwrappedImages unwrappedReduced meanGreenImages meanRedImages] = volumeImagingKmeans(nkmeans,volsPerOdor,nchannels,nvolseries,replicates,fractionOfVarianceTokeep);
+            cd([startDir '\' currFolders(i).name])
+        end
     end
     cd(startDir)
 end
@@ -28,8 +40,9 @@ disp(['done calculating kmeans.'])
 
 %% run makeClusters.m and processVolumeImagingKmeans.m for all folders
 
-folderToRunKmeansOn='Volumes';
-rawKmeansOutput='rawKmeans_80clusters_0.99fractionOfVarianceKept_50replicates.mat';
+folderToRunKmeansOn{1}='Volumes';
+folderToRunKmeansOn{2}='Volumes2';
+rawKmeansOutput='rawKmeans_80clusters_0.75fractionOfVarianceKept_50replicates.mat';
 clusterVolFile='clusterVols.mat';
 volumeAcquisitionTime=1.2;
 nChannels=2;
@@ -45,16 +58,20 @@ currFolders(~actuallyAFolder)=[];
 for i=1:length(currFolders)
     cd(currFolders(i).name)
     if exist('leftLobe')
-        cd(['leftLobe\' folderToRunKmeansOn])
-       [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput);
-       [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
-        cd([startDir '\' currFolders(i).name])
+        for j=1:length(folderToRunKmeansOn)
+            cd(['leftLobe\' folderToRunKmeansOn{j}])
+            [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput);
+            [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
+            cd([startDir '\' currFolders(i).name])
+        end
     end
     if exist('rightLobe')
-        cd(['rightLobe\'  folderToRunKmeansOn])
-       [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput);
-       [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
-        cd([startDir '\' currFolders(i).name])
+        for j=1:length(folderToRunKmeansOn)
+            cd(['rightLobe\'  folderToRunKmeansOn{j}])
+            [clusterVolU clusterInfoU]=makeClusters(rawKmeansOutput);
+            [grnResponse t] = processVolumeImagingKmeans(clusterVolFile,rawKmeansOutput,volumeAcquisitionTime,nChannels);
+            cd([startDir '\' currFolders(i).name])
+        end
     end
     cd(startDir)
 end
