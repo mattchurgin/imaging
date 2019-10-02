9118f28619cd519d564c81dbf65cdd3125b84748
Matt Churgin <mattchurgin@gmail.com>
2019-10-02 19:45:49 -0400

    deleted unused files

--- plotALResponse.m
+++ plotALResponse.m
@@ -3,6 +3,7 @@ function [] = plotALResponse(grnResponse,t,odors)
 % t specifies the time vector
 % odors is a vector specifying which odors to present the response for
 % if odors is not specified, all odor responses will be shown
+allColorMaps
 
 if nargin<3
     odors=1:size(grnResponse,1);
@@ -13,6 +14,7 @@ for i=1:length(odors)
     subplot(1,length(odors),i)
     
     imagesc(t,1:size(grnResponse,2),squeeze(grnResponse(odors(i),:,:)),[0 1])
+    
     title(['odor ' num2str(i)])
     if i==1
         xlabel('Time (s)')
